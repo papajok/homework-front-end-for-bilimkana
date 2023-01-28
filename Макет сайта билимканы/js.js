c774832e0df24991c1fc193bfe852152cd79ff03
luvkarina <116797390+papajok@users.noreply.github.com>
2023-01-28 17:05:09 +0600

    Add files via upload

---
+++ Макет сайта билимканы/js.js
@@ -0,0 +1,52 @@
+
+
+  new Swiper('.swiper__hero', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+      delay: 8000,
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+
+  new Swiper('.swiper__news', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+  
+    // Navigation arrows
+    autoplay: {
+      delay: 3000,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+ 
+  });
+  
+  new Swiper('.swiper__proj', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    autoplay: {
+      delay: 5000,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+ 
+  });
